72c6df58c18fcd900b8efc62c0f24fca06c4893d
Andresguc <Andresguc@gmail.com>
2023-06-02 08:46:30 -0500

    Adding the base configuration files to the game implement the canvas implement the map render function

--- game.js
+++ game.js
@@ -1,1 +1,49 @@
-console.log(maps);
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('load', setCanvasSize);
+    window.addEventListener('resize', setCanvasSize);
+
+    const canvas = document.querySelector('#game');
+
+    let canvasSize;
+    let elementsSize;
+
+    function setCanvasSize() {
+        if (window.innerHeight > window.innerWidth) {
+            canvasSize = window.innerWidth * 0.8;
+        } else {
+            canvasSize = window.innerHeight * 0.8;
+        }
+
+        canvas.setAttribute('width', canvasSize);
+        canvas.setAttribute('height', canvasSize);
+
+        elementsSize = canvasSize / 10;
+
+        startGame();
+    }
+
+    function startGame() {
+        const canvas = document.querySelector('#game');
+        const game = canvas.getContext('2d');
+
+        game.font = elementsSize + 'px Verdana';
+        game.textAlign = 'end';
+
+
+        const map = maps[0];
+        const mapRows = map.trim().split('\n');
+        const mapRowCols = mapRows.map(row => row.trim().split(''));
+        // console.log({ map, mapRows, mapRowCols });
+
+        mapRowCols.forEach((row, rowI) => {
+            row.forEach((col, colI) => {
+                const emoji = emojis[col];
+                const posX = elementsSize * (colI + 1);
+                const posY = elementsSize * (rowI + 1);;
+
+                game.fillText(emoji, posX, posY)
+                console.log({row, rowI, col, colI});
+            });
+        });
+    }
+});
--- maps.js
+++ maps.js
@@ -26,28 +26,82 @@ maps.push(`
     -XXXXXXXXX
     -XXXXXXXXX
     OXXXXXXXXX
-  `);
+`);
+
+maps.push(`
+    I------XXXX
+    -XXXXX---XX
+    XXXXXX-XXXX
+    XX-----XXXX
+    XXXXX---XXX
+    XX----XXXXX
+    X--XXXX-XXX
+    X-XXX--XXXX
+    X--------X
+    XXXXXXXXOX
+`);
+
+maps.push(`
+    O-----XXXX
+    X-XXX-XXXX
+    XX-----XXX
+    XXXX-XXXXX
+    X--------X
+    XX--XXXX-O
+    XX----XXXX
+    XXXX-----X
+    X----XXXIX
+    XXXXXXXXXX
+`);
+
 maps.push(`
     O--XXXXXXX
-    X--XXXXXXX
-    XX----XXXX
-    X--XX-XXXX
-    X-XXX--XXX
+    X--------X
+    X--XXX-XXX
+    XXXXX---XX
     X-XXXX-XXX
-    XX--XX--XX
-    XX--XXX-XX
-    XXXX---IXX
+    XX-------X
+    XX-XXXXX-O
+    X----XXX-X
+    X--X--XX-I
     XXXXXXXXXX
-    `);
+`);
+
 maps.push(`
-    I-----XXXX
-    XXXXX-XXXX
-    XX----XXXX
+    I----XXXXX
+    XX---X--XX
     XX-XXXXXXX
-    XX-----XXX
-    XXXXXX-XXX
-    XX-----XXX
-    XX-XXXXXXX
-    XX-----OXX
+    X-----X--X
+    XXXX-XXX-X
+    X-------OX
+    XXXX--XXXX
+    XX---X--XX
+    X-XX-XXXXX
     XXXXXXXXXX
-  `);
+`);
+
+maps.push(`
+    I-XXXXXXXX
+    X---XXXX-X
+    X-X-XX--XX
+    X-X------X
+    XX--XX-XXX
+    X--XXX-X-X
+    XX-XXXX-OX
+    X-------XX
+    X-XX-XXXXX
+    XXXXXXXXXX
+`);
+
+maps.push(`
+    O-XXXXXXXX
+    X------XXX
+    XX--X-XXXX
+    X--X--X--X
+    X-XX-XXX-X
+    XX-------X
+    X--XXXX--X
+    X-XX-XXXXI
+    X---------
+    XXXXXXXXXX
+`);
